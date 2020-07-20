f97e86be91da6e403f3f513cafd841e9c8c36bdc
liamocuz <liamo@me.com>
2020-07-20 14:38:39 -0400

    Added the isOccupied and teammateOccupied methods

--- src/Piece.java
+++ src/Piece.java
@@ -18,4 +18,18 @@ public abstract class Piece {
 
     public boolean isAlive() {return this.alive;}
     public void setAlive(boolean status) {this.alive = status;}
+
+    // Used to check if
+    protected boolean isOccupied (Spot check)
+    {
+        // true = there is a piece on that spot
+        // false = no piece
+        return check.getPiece() != null;
+    }
+
+    protected boolean teammateOccupied (Spot check)
+    {
+        // Checks that spot piece is not null and that both piece colors are the same
+        return !isOccupied(check) && check.getPiece().getPlayer().getColor() == this.getPlayer().getColor();
+    }
 }
